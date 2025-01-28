76ce428313e3bc31620011f4585ad23dad2ade1b
Sergey Strigin <darkauron1997@gmail.com>
2025-01-28 17:21:31 +0600

    update route name

--- load.php
+++ load.php
@@ -2,20 +2,20 @@
 /**
  * Plugin Name: Icreative Wordpress Details
  * Description: Creates a Rest API route to retrieve wordpress core, theme and plugins versions of the website.
- * Version: 1.0.2
+ * Version: 1.1.0
  * Author: Icreative
  */
 
-function ic_token_auth($request): bool
+function ic_details_token_auth($request): bool
 {
     return $request->get_header('x-api-key') === API_KEY;
 }
 
 add_action('rest_api_init', function () {
-    register_rest_route('api/v1', '/wp-info', array(
+    register_rest_route('api/v1', '/icreative/wordpress-details', array(
         'methods' => 'GET',
         'callback' => 'handle_get_wp_info_request',
-        'permission_callback' => 'ic_token_auth',
+        'permission_callback' => 'ic_details_token_auth',
     ));
 });
 
